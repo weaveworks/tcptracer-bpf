dd0b8e1ff3bed3961ebb4e9e8db7a8f7396d3b55
Bryan Boreham <bjboreham@gmail.com>
2020-01-14 14:33:08 +0000

    Added doc.go file so go mod copies C files

---
+++ doc.go
@@ -0,0 +1,7 @@
+// There is no Go code in this directory, but we need the generated .c
+// and .h files when the library is re-used, so this file exists to
+// get `go mod` to copy those files.
+
+package tcptracer_bpf
+
+import "C"
